7f9d41bad2e8c22143a31b27f1c38fbeb4fd6bd5
davidpalau <palau.david@hotmail.com>
2022-09-24 11:58:04 +0200

    preparado extructura

---
+++ js/acti_6.js
@@ -0,0 +1,15 @@
+/*❑ ACTIVIDAD 6 – Juego de adivinanzas de números
+- Hacer una página web que implemente un juego de encontrar un número aleatorio bajo las premisas
+se explica a continuación:
+o La página calculará un número del 1 al 100
+o Luego le pedirá al usuario el número
+o Si el usuario escribe algo que no es un número, se indica el error y el
+Se solicita nuevamente el número.
+o Si el número introducido por el usuario es correcto, se indica que era correcto y
+terminaremos el juego
+o Si no, te dice si el número es menor o mayor y vuelve a preguntar cuál es
+o Si se cancela alguna casilla, el juego termina indicando que el juego fue cancelado.
+o Al final, si el juego se ha completado con éxito, el número de intentos es
+indicado
+o El usuario puede volver a jugar usando un cuadro de confirmación
+*/
