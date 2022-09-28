5314c5dfdb66e261a897467cd553cae07c45845e
davidpalau <palau.david@hotmail.com>
2022-09-28 20:25:28 +0200

    actividad 11

--- js/acti_11.js
+++ js/acti_11.js
@@ -5,3 +5,13 @@
 - Example of result:
 
 */
+  // crea un nuevo div
+  // y añade contenido
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode("Hola!¿Qué tal?");
+  newDiv.appendChild(newContent); //añade texto al div creado.
+
+  // añade el elemento creado y su contenido al DOM
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+  
