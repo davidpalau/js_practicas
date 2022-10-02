39d0ed9a42d054299073196ba2ec5e76601d3cac
davidpalau <palau.david@hotmail.com>
2022-10-02 16:48:03 +0200

    El 10 terminado css

--- js/acti_10.js
+++ js/acti_10.js
@@ -23,7 +23,7 @@ function genera_tabla() {
       // Crea las hileras de la tabla
       var hilera = document.createElement("tr");
   
-      for (var j = 0; j < 15; j++) {
+      for (var j = 0; j < 10; j++) {
         // Crea un elemento <td> y un nodo de texto, haz que el nodo de
         // texto sea el contenido de <td>, ubica el elemento <td> al final
         // de la hilera de la tabla
