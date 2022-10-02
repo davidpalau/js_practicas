269d857423a91a9823acddb64236a0f8c4be78d2
davidpalau <palau.david@hotmail.com>
2022-10-02 16:16:20 +0200

    El 10 terminado

--- js/acti_10.js
+++ js/acti_10.js
@@ -10,3 +10,43 @@ character of that code.
 carácter de ese código.
 - Cada fila mostrará 10 símbolos
 */
+function genera_tabla() {
+    // Obtener la referencia del elemento body
+    var body = document.getElementsByTagName("body")[0];
+  
+    // Crea un elemento <table> y un elemento <tbody>
+    var tabla   = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+  
+    // Crea las celdas
+    for (var i = 1; i < 10000; i++) {
+      // Crea las hileras de la tabla
+      var hilera = document.createElement("tr");
+  
+      for (var j = 0; j < 15; j++) {
+        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // de la hilera de la tabla
+        var celda1 = document.createElement("td");
+        var celda2 = document.createElement("td");
+        let char = String.fromCharCode(i);
+        var textoCelda = document.createTextNode(i+" ");
+        celda1.appendChild(textoCelda);
+        textoCelda = document.createTextNode(char+" ");
+        celda2.appendChild(textoCelda);
+        hilera.appendChild(celda1);
+        hilera.appendChild(celda2);
+        i++;
+      }
+      i--;
+      // agrega la hilera al final de la tabla (al final del elemento tblbody)
+      tblBody.appendChild(hilera);
+    }
+  
+    // posiciona el <tbody> debajo del elemento <table>
+    tabla.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tabla);
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
+  }
